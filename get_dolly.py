a1c6e97ec9003173ac607647597bcd2586003557
Colby Ford <colbytylerford@gmail.com>
2023-04-22 17:47:33 -0400

    Add 7B and 12B models in get_dolly.py

--- get_dolly.py
+++ get_dolly.py
@@ -1,7 +1,22 @@
 from transformers import AutoModelForCausalLM, AutoTokenizer
 
+## 3B Model (~5GB)
 tokenizer = AutoTokenizer.from_pretrained("databricks/dolly-v2-3b", padding_side="left")
 tokenizer.save_pretrained('/home/site/wwwroot/dolly/tokenizer')
 
 model = AutoModelForCausalLM.from_pretrained("databricks/dolly-v2-3b", device_map="auto")
 model.save_pretrained('/home/site/wwwroot/dolly/model')
+
+## 7B Model (~14GB)
+# tokenizer = AutoTokenizer.from_pretrained("databricks/dolly-v2-7b", padding_side="left")
+# tokenizer.save_pretrained('/home/site/wwwroot/dolly/tokenizer')
+
+# model = AutoModelForCausalLM.from_pretrained("databricks/dolly-v2-7b", device_map="auto")
+# model.save_pretrained('/home/site/wwwroot/dolly/model')
+
+## 12B Model (~24GB)
+# tokenizer = AutoTokenizer.from_pretrained("databricks/dolly-v2-12b", padding_side="left")
+# tokenizer.save_pretrained('/home/site/wwwroot/dolly/tokenizer')
+
+# model = AutoModelForCausalLM.from_pretrained("databricks/dolly-v2-12b", device_map="auto")
+# model.save_pretrained('/home/site/wwwroot/dolly/model')
