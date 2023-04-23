0f893516046e700cdeeff04cd0955b01e7096b44
Colby Ford <colbytylerford@gmail.com>
2023-04-23 00:28:10 -0400

    Simplify get_dolly.py script

--- get_dolly.py
+++ get_dolly.py
@@ -2,21 +2,17 @@ from transformers import AutoModelForCausalLM, AutoTokenizer
 
 ## 3B Model (~5GB)
 tokenizer = AutoTokenizer.from_pretrained("databricks/dolly-v2-3b", padding_side="left")
-tokenizer.save_pretrained('/home/site/wwwroot/dolly/tokenizer')
-
 model = AutoModelForCausalLM.from_pretrained("databricks/dolly-v2-3b", device_map="auto")
-model.save_pretrained('/home/site/wwwroot/dolly/model')
 
 ## 7B Model (~14GB)
 # tokenizer = AutoTokenizer.from_pretrained("databricks/dolly-v2-7b", padding_side="left")
-# tokenizer.save_pretrained('/home/site/wwwroot/dolly/tokenizer')
-
 # model = AutoModelForCausalLM.from_pretrained("databricks/dolly-v2-7b", device_map="auto")
-# model.save_pretrained('/home/site/wwwroot/dolly/model')
 
 ## 12B Model (~24GB)
 # tokenizer = AutoTokenizer.from_pretrained("databricks/dolly-v2-12b", padding_side="left")
-# tokenizer.save_pretrained('/home/site/wwwroot/dolly/tokenizer')
-
 # model = AutoModelForCausalLM.from_pretrained("databricks/dolly-v2-12b", device_map="auto")
-# model.save_pretrained('/home/site/wwwroot/dolly/model')
+
+## Save Tokenizer and Model
+tokenizer.save_pretrained('/home/site/wwwroot/dolly/tokenizer')
+model.save_pretrained('/home/site/wwwroot/dolly/model')
+
