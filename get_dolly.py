ef679c0892b06a959c6f4c7a38a92cb09fac3853
Colby Ford <colbytylerford@gmail.com>
2023-04-23 13:06:28 -0400

    Update get_dolly.py

--- get_dolly.py
+++ get_dolly.py
@@ -1,14 +1,14 @@
 from transformers import AutoModelForCausalLM, AutoTokenizer
 
-## 3B Model (~5GB)
+## 3B Model (about 5GB)
 tokenizer = AutoTokenizer.from_pretrained("databricks/dolly-v2-3b", padding_side="left")
 model = AutoModelForCausalLM.from_pretrained("databricks/dolly-v2-3b", device_map="auto")
 
-## 7B Model (~14GB)
+## 7B Model (about 14GB)
 # tokenizer = AutoTokenizer.from_pretrained("databricks/dolly-v2-7b", padding_side="left")
 # model = AutoModelForCausalLM.from_pretrained("databricks/dolly-v2-7b", device_map="auto")
 
-## 12B Model (~24GB)
+## 12B Model (about 24GB)
 # tokenizer = AutoTokenizer.from_pretrained("databricks/dolly-v2-12b", padding_side="left")
 # model = AutoModelForCausalLM.from_pretrained("databricks/dolly-v2-12b", device_map="auto")
 
